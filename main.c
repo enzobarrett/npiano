2c15b4e163d6bfb790e3067caa224040a0a4e21f
Enzo Barrett <enzobarrett@gmail.com>
2019-01-25 11:19:24 -0700

    Comment out fun feature because of Xander

--- main.c
+++ main.c
@@ -239,11 +239,11 @@ int main()
 		set(y, x, c, 1);
 		play(A_1, leng);
 		break;
-	    case '0':
-		//Play a surprise 
+	    /*case '0':
+		Play a surprise 
 		system("beep -f 130 -l 100 -n -f 262 -l 100 -n -f 330 -l 100 -n -f 392 -l 100 -n -f 523 -l 100 -n -f 660 -l 100 -n -f 784 -l 300 -n -f 660 -l 300 -n -f 146 -l 100 -n -f 262 -l 100 -n -f 311 -l 100 -n -f 415 -l 100 -n -f 523 -l 100 -n -f 622 -l 100 -n -f 831 -l 300 -n -f 622 -l 300 -n -f 155 -l 100 -n -f 294 -l 100 -n -f 349 -l 100 -n -f 466 -l 100 -n -f 588 -l 100 -n -f 699 -l 100 -n -f 933 -l 300 -n -f 933 -l 100 -n -f 933 -l 100 -n -f 933 -l 100 -n -f 1047 -l 400");
 		y = -1;
-		x = -1;
+		x = -1;*/
 	    default:
 		y = -1;
 		x = -1;
